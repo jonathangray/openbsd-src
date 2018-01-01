7764d2862b33da2a94ccdf20dfa2ebbf53cdda2f
Jonathan Gray <jsg@jsg.id.au>
2018-01-01 22:23:30 +1100

    adjust for difference PAGE_MASK

--- sys/dev/pci/drm/radeon/radeon_ttm.c
+++ sys/dev/pci/drm/radeon/radeon_ttm.c
@@ -1104,7 +1104,7 @@ static ssize_t radeon_ttm_gtt_read(struct file *f, char __user *buf,
 
 	while (size) {
 		loff_t p = *pos / PAGE_SIZE;
-		unsigned off = *pos & ~PAGE_MASK;
+		unsigned off = *pos & PAGE_MASK;
 		size_t cur_size = min_t(size_t, size, PAGE_SIZE - off);
 		struct vm_page *page;
 		void *ptr;
