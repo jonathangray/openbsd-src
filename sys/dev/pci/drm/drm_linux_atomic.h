6f4b10945f0493e64d2f9221b65a647b551a9f58
Jonathan Gray <jsg@jsg.id.au>
2018-11-03 15:56:45 +1100

    move barriers to drm_linux_atomic.h

--- sys/dev/pci/drm/drm_linux_atomic.h
+++ sys/dev/pci/drm/drm_linux_atomic.h
@@ -312,4 +312,57 @@ find_next_bit(volatile void *p, int max, int b)
 	     (b) < (max);					\
 	     (b) = find_next_bit((p), (max), (b) + 1))
 
+/* DRM_READMEMORYBARRIER() prevents reordering of reads.
+ * DRM_WRITEMEMORYBARRIER() prevents reordering of writes.
+ * DRM_MEMORYBARRIER() prevents reordering of reads and writes.
+ */
+#if defined(__i386__)
+#define DRM_READMEMORYBARRIER()		__asm __volatile( \
+					"lock; addl $0,0(%%esp)" : : : "memory");
+#define DRM_WRITEMEMORYBARRIER()	__asm __volatile("" : : : "memory");
+#define DRM_MEMORYBARRIER()		__asm __volatile( \
+					"lock; addl $0,0(%%esp)" : : : "memory");
+#elif defined(__alpha__)
+#define DRM_READMEMORYBARRIER()		alpha_mb();
+#define DRM_WRITEMEMORYBARRIER()	alpha_wmb();
+#define DRM_MEMORYBARRIER()		alpha_mb();
+#elif defined(__amd64__)
+#define DRM_READMEMORYBARRIER()		__asm __volatile( \
+					"lock; addl $0,0(%%rsp)" : : : "memory");
+#define DRM_WRITEMEMORYBARRIER()	__asm __volatile("" : : : "memory");
+#define DRM_MEMORYBARRIER()		__asm __volatile( \
+					"lock; addl $0,0(%%rsp)" : : : "memory");
+#elif defined(__aarch64__)
+#define DRM_READMEMORYBARRIER()		__membar("dsb ld")
+#define DRM_WRITEMEMORYBARRIER()	__membar("dsb st")
+#define DRM_MEMORYBARRIER()		__membar("dsb sy")
+#elif defined(__mips64__)
+#define DRM_READMEMORYBARRIER()		DRM_MEMORYBARRIER() 
+#define DRM_WRITEMEMORYBARRIER()	DRM_MEMORYBARRIER()
+#define DRM_MEMORYBARRIER()		mips_sync()
+#elif defined(__powerpc__)
+#define DRM_READMEMORYBARRIER()		DRM_MEMORYBARRIER() 
+#define DRM_WRITEMEMORYBARRIER()	DRM_MEMORYBARRIER()
+#define DRM_MEMORYBARRIER()		__asm __volatile("sync" : : : "memory");
+#elif defined(__sparc64__)
+#define DRM_READMEMORYBARRIER()		DRM_MEMORYBARRIER() 
+#define DRM_WRITEMEMORYBARRIER()	DRM_MEMORYBARRIER()
+#define DRM_MEMORYBARRIER()		membar_sync()
+#endif
+
+#define smp_mb__before_atomic()		DRM_MEMORYBARRIER()
+#define smp_mb__before_atomic_dec()	DRM_MEMORYBARRIER()
+#define smp_mb__after_atomic_dec()	DRM_MEMORYBARRIER()
+#define smp_mb__before_atomic_inc()	DRM_MEMORYBARRIER()
+#define smp_mb__after_atomic_inc()	DRM_MEMORYBARRIER()
+
+#define smp_store_mb(x, v)		do { x = v; DRM_MEMORYBARRIER(); } while (0)
+
+#define mb()				DRM_MEMORYBARRIER()
+#define rmb()				DRM_READMEMORYBARRIER()
+#define wmb()				DRM_WRITEMEMORYBARRIER()
+#define smp_rmb()			DRM_READMEMORYBARRIER()
+#define smp_wmb()			DRM_WRITEMEMORYBARRIER()
+#define mmiowb()			DRM_WRITEMEMORYBARRIER()
+
 #endif
