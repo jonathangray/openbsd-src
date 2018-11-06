6c7702b558d1e73358f4c025191d0c1f1103d0ea
Jonathan Gray <jsg@jsg.id.au>
2018-11-06 22:57:27 +1100

    add mutex_lock_nested

--- sys/dev/pci/drm/drm_linux.h
+++ sys/dev/pci/drm/drm_linux.h
@@ -611,6 +611,7 @@ _spin_unlock_irqrestore(struct mutex *mtxp, __unused unsigned long flags
 #define mutex_lock_interruptible(rwl)	-rw_enter(rwl, RW_WRITE | RW_INTR)
 #define mutex_lock(rwl)			rw_enter_write(rwl)
 #define mutex_lock_nest_lock(rwl, sub)	rw_enter_write(rwl)
+#define mutex_lock_nested(rwl, sub)	rw_enter_write(rwl)
 #define mutex_trylock(rwl)		(rw_enter(rwl, RW_WRITE | RW_NOSLEEP) == 0)
 #define mutex_unlock(rwl)		rw_exit_write(rwl)
 #define mutex_is_locked(rwl)		(rw_status(rwl) == RW_WRITE)
