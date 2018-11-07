44178824cd7273aa2488096c5d341b26a08f1a4a
Jonathan Gray <jsg@jsg.id.au>
2018-11-07 16:00:43 +1100

    add dma_fence_array

--- sys/dev/pci/drm/drm_linux.h
+++ sys/dev/pci/drm/drm_linux.h
@@ -2098,6 +2098,23 @@ dma_fence_set_error(struct dma_fence *fence, int error)
 	fence->error = error;
 }
 
+struct dma_fence_array {
+	unsigned int num_fences;
+	struct dma_fence **fences;
+};
+
+static inline struct dma_fence_array *
+to_dma_fence_array(struct dma_fence *fence)
+{
+	return NULL;
+}
+
+static inline bool
+dma_fence_is_array(struct dma_fence *fence)
+{
+	return false;
+}
+
 struct sync_file {
 };
 
