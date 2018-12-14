e131fe6f653b27000ae1a703c8dead494191bf1f
Jonathan Gray <jsg@jsg.id.au>
2018-12-14 19:11:59 +1100

    add hash_64 hash_long

--- sys/dev/pci/drm/include/linux/hash.h
+++ sys/dev/pci/drm/include/linux/hash.h
@@ -14,4 +14,19 @@ hash_32(uint32_t val, unsigned int bits)
 	return (val * GOLDEN_RATIO_32) >> (32 - bits);
 }
 
+/* 2^64 * ((sqrt(5) - 1) / 2) from Knuth */
+#define GOLDEN_RATIO_64 0x9e3779b97f4a7c16ULL
+
+static inline uint32_t
+hash_64(uint64_t val, unsigned int bits)
+{
+	return (val * GOLDEN_RATIO_64) >> (64 - bits);
+}
+
+#ifdef __LP64__
+#define hash_long(val, bits) hash_64(val, bits)
+#else
+#define hash_long(val, bits) hash_32(val, bits)
+#endif
+
 #endif
