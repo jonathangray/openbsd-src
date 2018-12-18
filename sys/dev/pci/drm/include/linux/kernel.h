64680ec08790fd260311a806f9691ee0197a85bf
Jonathan Gray <jsg@jsg.id.au>
2018-12-18 15:41:55 +1100

    add defines for signed fixed width max values

--- sys/dev/pci/drm/include/linux/kernel.h
+++ sys/dev/pci/drm/include/linux/kernel.h
@@ -27,11 +27,16 @@
 	const __typeof( ((type *)0)->member ) *__mptr = (ptr);	\
 	(type *)( (char *)__mptr - offsetof(type,member) );})
 
-#define U8_MAX UINT8_MAX
-#define U16_MAX UINT16_MAX
-#define U32_MAX UINT32_MAX
-#define U64_C(x) UINT64_C(x)
-#define U64_MAX UINT64_MAX
+#define S8_MAX		INT8_MAX
+#define S16_MAX		INT16_MAX
+#define S32_MAX		INT32_MAX
+#define S64_MAX		INT64_MAX
+
+#define U8_MAX		UINT8_MAX
+#define U16_MAX		UINT16_MAX
+#define U32_MAX		UINT32_MAX
+#define U64_C(x)	UINT64_C(x)
+#define U64_MAX		UINT64_MAX
 
 #define IS_ALIGNED(x, y)	(((x) & ((y) - 1)) == 0)
 
