37b2e4baddd24584c7278d56092fe29b386b7054
Jonathan Gray <jsg@jsg.id.au>
2019-02-08 19:15:57 +1100

    set connector backlight property in atomic path
    There are local changes to set/use a backlight property on connectors.
    Add an equivalent of the #ifdef OpenBSD part of
    drm_connector_set_obj_prop() to set_property_atomic() to make
    setting the backlight via xbacklight with the modesetting xorg driver
    work.
    
    While it is possible to do this early in drm_mode_obj_set_property_ioctl()
    and avoid duplication, care would have to be taken to get locking etc
    right.
    
    non-atomic modesetting path
    
    drm_mode_obj_set_property_ioctl
    set_property_legacy
    drm_connector_set_obj_prop
    
    atomic modesetting path
    
    drm_mode_obj_set_property_ioctl
    set_property_atomic

--- sys/dev/pci/drm/drm_mode_object.c
+++ sys/dev/pci/drm/drm_mode_object.c
@@ -493,6 +493,14 @@ retry:
 		ret = drm_atomic_connector_commit_dpms(state,
 						       obj_to_connector(obj),
 						       prop_value);
+#ifdef __OpenBSD__
+	} else if (obj->type == DRM_MODE_OBJECT_CONNECTOR &&
+	    prop == (obj_to_connector(obj))->backlight_property) {
+		ret = 0;
+		struct drm_connector *connector = obj_to_connector(obj);
+		connector->backlight_device->props.brightness = prop_value;
+		backlight_schedule_update_status(connector->backlight_device);
+#endif
 	} else {
 		ret = drm_atomic_set_property(state, obj, prop, prop_value);
 		if (ret)
