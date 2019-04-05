e2fcb7748ab8079e57c0e3183d49097720c3a450
Jonathan Gray <jsg@jsg.id.au>
2019-04-05 11:30:40 +1100

    add stubbed dma_fence_array_create()

--- sys/dev/pci/drm/include/linux/dma-fence-array.h
+++ sys/dev/pci/drm/include/linux/dma-fence-array.h
@@ -5,7 +5,14 @@
 
 #include <linux/dma-fence.h>
 
+#ifndef STUB
+#include <sys/types.h>
+#include <sys/systm.h>
+#define STUB() do { printf("%s: stub\n", __func__); } while(0)
+#endif
+
 struct dma_fence_array {
+	struct dma_fence base;
 	unsigned int num_fences;
 	struct dma_fence **fences;
 };
@@ -20,6 +27,14 @@ static inline bool
 dma_fence_is_array(struct dma_fence *fence)
 {
 	return false;
+}
+
+static inline struct dma_fence_array *
+dma_fence_array_create(int num_fences, struct dma_fence **fences, u64 context,
+    unsigned seqno, bool signal_on_any)
+{
+	STUB();
+	return NULL;
 }
 
 #endif
