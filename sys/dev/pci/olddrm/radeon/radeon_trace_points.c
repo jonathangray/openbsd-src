55bbe151258a8e2f2dd09dfc82818d3a7e71cf55
Jonathan Gray <jsg@jsg.id.au>
2019-06-03 12:46:34 +1000

    add drm dir from 20190412 as olddrm
    changed for different path
    
    adapted witness mtx wrapper changes from
    8c00de5e511bb647ae228590153001775e12ff38

---
+++ sys/dev/pci/olddrm/radeon/radeon_trace_points.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+/* Copyright Red Hat Inc 2010.
+ * Author : Dave Airlie <airlied@redhat.com>
+ */
+#include <dev/pci/olddrm/drmP.h>
+#include <dev/pci/olddrm/radeon_drm.h>
+#include "radeon.h"
+
+#define CREATE_TRACE_POINTS
+#include "radeon_trace.h"
