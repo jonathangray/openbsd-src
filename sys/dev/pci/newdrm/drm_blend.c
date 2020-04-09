60c4981fc49e06abf88e6a977f9cfffda95330ea
Jonathan Gray <jsg@jsg.id.au>
2020-04-09 14:56:28 +1000

    make drm_blend.c build

--- sys/dev/pci/newdrm/drm_blend.c
+++ sys/dev/pci/newdrm/drm_blend.c
@@ -427,6 +427,9 @@ static int drm_atomic_state_zpos_cmp(const void *a, const void *b)
 static int drm_atomic_helper_crtc_normalize_zpos(struct drm_crtc *crtc,
 					  struct drm_crtc_state *crtc_state)
 {
+	STUB();
+	return -ENOSYS;
+#ifdef notyet
 	struct drm_atomic_state *state = crtc_state->state;
 	struct drm_device *dev = crtc->dev;
 	int total_planes = dev->mode_config.num_total_plane;
@@ -473,6 +476,7 @@ static int drm_atomic_helper_crtc_normalize_zpos(struct drm_crtc *crtc,
 done:
 	kfree(states);
 	return ret;
+#endif
 }
 
 /**
