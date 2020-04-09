fc1e50fbbbb0d2f19739637fdd586b13280a6735
Jonathan Gray <jsg@jsg.id.au>
2020-04-09 15:02:56 +1000

    lock_release() lost an argument in linux 5.5

--- sys/dev/pci/newdrm/include/linux/lockdep.h
+++ sys/dev/pci/newdrm/include/linux/lockdep.h
@@ -9,7 +9,7 @@ struct lock_class_key {
 #define might_lock(lock)
 #define lockdep_assert_held(lock)	do { (void)(lock); } while(0)
 #define lock_acquire(lock, a, b, c, d, e, f)
-#define lock_release(lock, a, b)
+#define lock_release(lock, a)
 #define lock_acquire_shared_recursive(lock, a, b, c, d)
 #define lockdep_set_subclass(a, b)
 
