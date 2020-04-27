f7b1b3d95075a431637dd9d8ba9d04b2d957ddd8
Jonathan Gray <jsg@jsg.id.au>
2020-04-27 12:37:18 +1000

    add DIV64_U64_ROUND_UP()

--- sys/dev/pci/newdrm/include/linux/math64.h
+++ sys/dev/pci/newdrm/include/linux/math64.h
@@ -56,4 +56,10 @@ mul_u64_u32_div(uint64_t x, uint32_t y, uint32_t div)
 	return (x * y) / div;
 }
 
+#define DIV64_U64_ROUND_UP(x, y)	\
+({					\
+	uint64_t _t = (y);		\
+	div64_u64((x) + _t - 1, _t);	\
+})
+
 #endif
