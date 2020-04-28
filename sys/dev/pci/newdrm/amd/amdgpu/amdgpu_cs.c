46ceaada27679d796aa53c7e96f0110943cfaac7
Jonathan Gray <jsg@jsg.id.au>
2020-04-28 12:17:13 +1000

    make amdgpu_cs.c build

--- sys/dev/pci/newdrm/amd/amdgpu/amdgpu_cs.c
+++ sys/dev/pci/newdrm/amd/amdgpu/amdgpu_cs.c
@@ -479,9 +479,11 @@ static int amdgpu_cs_list_validate(struct amdgpu_cs_parser *p,
 		struct amdgpu_bo *bo = ttm_to_amdgpu_bo(lobj->tv.bo);
 		struct mm_struct *usermm;
 
+#ifdef notyet
 		usermm = amdgpu_ttm_tt_get_usermm(bo->tbo.ttm);
 		if (usermm && usermm != current->mm)
 			return -EPERM;
+#endif
 
 		if (amdgpu_ttm_tt_is_userptr(bo->tbo.ttm) &&
 		    lobj->user_invalidated && lobj->user_pages) {
@@ -1422,6 +1424,9 @@ static struct dma_fence *amdgpu_cs_get_fence(struct amdgpu_device *adev,
 int amdgpu_cs_fence_to_handle_ioctl(struct drm_device *dev, void *data,
 				    struct drm_file *filp)
 {
+	STUB();
+	return -ENOSYS;
+#ifdef notyet
 	struct amdgpu_device *adev = dev->dev_private;
 	union drm_amdgpu_fence_to_handle *info = data;
 	struct dma_fence *fence;
@@ -1476,6 +1481,7 @@ int amdgpu_cs_fence_to_handle_ioctl(struct drm_device *dev, void *data,
 	default:
 		return -EINVAL;
 	}
+#endif
 }
 
 /**
