03c442f9080f061606119bb51d86f50c470c66a8
Jonathan Gray <jsg@jsg.id.au>
2020-04-28 13:29:20 +1000

    make amdgpu_fence.c build

--- sys/dev/pci/newdrm/amd/amdgpu/amdgpu_fence.c
+++ sys/dev/pci/newdrm/amd/amdgpu/amdgpu_fence.c
@@ -57,22 +57,31 @@ struct amdgpu_fence {
 	struct amdgpu_ring		*ring;
 };
 
-static struct kmem_cache *amdgpu_fence_slab;
+static struct pool amdgpu_fence_slab;
 
 int amdgpu_fence_slab_init(void)
 {
+#ifdef __linux__
 	amdgpu_fence_slab = kmem_cache_create(
 		"amdgpu_fence", sizeof(struct amdgpu_fence), 0,
 		SLAB_HWCACHE_ALIGN, NULL);
 	if (!amdgpu_fence_slab)
 		return -ENOMEM;
+#else
+	pool_init(&amdgpu_fence_slab, sizeof(struct amdgpu_fence),
+	    0, IPL_TTY, 0, "amdgpu_fence", NULL);
+#endif
 	return 0;
 }
 
 void amdgpu_fence_slab_fini(void)
 {
 	rcu_barrier();
+#ifdef __linux__
 	kmem_cache_destroy(amdgpu_fence_slab);
+#else
+	pool_destroy(&amdgpu_fence_slab);
+#endif
 }
 /*
  * Cast helper
@@ -143,7 +152,11 @@ int amdgpu_fence_emit(struct amdgpu_ring *ring, struct dma_fence **f,
 	uint32_t seq;
 	int r;
 
+#ifdef __linux__
 	fence = kmem_cache_alloc(amdgpu_fence_slab, GFP_KERNEL);
+#else
+	fence = pool_get(&amdgpu_fence_slab, PR_WAITOK);
+#endif
 	if (fence == NULL)
 		return -ENOMEM;
 
@@ -290,10 +303,9 @@ bool amdgpu_fence_process(struct amdgpu_ring *ring)
  *
  * Checks for fence activity.
  */
-static void amdgpu_fence_fallback(struct timer_list *t)
+static void amdgpu_fence_fallback(void *arg)
 {
-	struct amdgpu_ring *ring = from_timer(ring, t,
-					      fence_drv.fallback_timer);
+	struct amdgpu_ring *ring = arg;
 
 	if (amdgpu_fence_process(ring))
 		DRM_WARN("Fence fallback timer expired on ring %s\n", ring->name);
@@ -450,7 +462,12 @@ int amdgpu_fence_driver_init_ring(struct amdgpu_ring *ring,
 	atomic_set(&ring->fence_drv.last_seq, 0);
 	ring->fence_drv.initialized = false;
 
+#ifdef __linux__
 	timer_setup(&ring->fence_drv.fallback_timer, amdgpu_fence_fallback, 0);
+#else
+	timeout_set(&ring->fence_drv.fallback_timer, amdgpu_fence_fallback,
+	    ring);
+#endif
 
 	ring->fence_drv.num_fences_mask = num_hw_submission * 2 - 1;
 	mtx_init(&ring->fence_drv.lock, IPL_TTY);
@@ -657,7 +674,11 @@ static void amdgpu_fence_free(struct rcu_head *rcu)
 {
 	struct dma_fence *f = container_of(rcu, struct dma_fence, rcu);
 	struct amdgpu_fence *fence = to_amdgpu_fence(f);
+#ifdef __linux__
 	kmem_cache_free(amdgpu_fence_slab, fence);
+#else
+	pool_put(&amdgpu_fence_slab, fence);
+#endif
 }
 
 /**
