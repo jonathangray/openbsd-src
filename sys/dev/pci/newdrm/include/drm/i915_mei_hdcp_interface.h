33f2a7cfc3a47d7f38747bb3c921a16ea9bd8386
Jonathan Gray <jsg@jsg.id.au>
2020-05-01 19:03:18 +1000

    make dvo_sil164.c build

---
+++ sys/dev/pci/newdrm/include/drm/i915_mei_hdcp_interface.h
@@ -0,0 +1,13 @@
+/* Public domain. */
+
+#ifndef _I915_MEI_HDCP_INTERFACE_H_
+#define _I915_MEI_HDCP_INTERFACE_H_
+
+enum hdcp_wired_protocol {
+	HDCP_PROTOCOL_INVALID
+};
+
+struct hdcp_port_data {
+};
+
+#endif
