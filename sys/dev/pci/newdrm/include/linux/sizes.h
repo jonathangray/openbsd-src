4eba4a67713de32c9022f9da3a888d3c4e9925d5
Jonathan Gray <jsg@jsg.id.au>
2020-05-04 12:41:15 +1000

    make intel_wopcm.c build

--- sys/dev/pci/newdrm/include/linux/sizes.h
+++ sys/dev/pci/newdrm/include/linux/sizes.h
@@ -3,11 +3,14 @@
 #ifndef _LINUX_SIZES_H
 #define _LINUX_SIZES_H
 
+#define SZ_1K	(1024 * 1)
 #define SZ_4K	(1024 * 4)
 #define SZ_8K	(1024 * 8)
+#define SZ_16K	(1024 * 16)
 #define SZ_32K	(1024 * 32)
 #define SZ_128K	(1024 * 128)
-#define SZ_1M	(1024 * 1024)
-#define SZ_16M	(16 * 1024 * 1024)
+#define SZ_1M	(1024 * 1024 * 1)
+#define SZ_2M	(1024 * 1024 * 2)
+#define SZ_16M	(1024 * 1024 * 16)
 
 #endif
