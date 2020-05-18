1b13ffe5d1c1b62b90c4472dfee13e5626379a89
Jonathan Gray <jsg@jsg.id.au>
2020-05-19 00:11:13 +1000

    unstub intel_runtime_pm_init_early()

--- sys/dev/pci/newdrm/i915/intel_runtime_pm.c
+++ sys/dev/pci/newdrm/i915/intel_runtime_pm.c
@@ -606,16 +606,15 @@ void intel_runtime_pm_driver_release(struct intel_runtime_pm *rpm)
 
 void intel_runtime_pm_init_early(struct intel_runtime_pm *rpm)
 {
-	STUB();
-#ifdef notyet
 	struct drm_i915_private *i915 =
 			container_of(rpm, struct drm_i915_private, runtime_pm);
+#ifdef notyet
 	struct pci_dev *pdev = i915->drm.pdev;
 	struct device *kdev = &pdev->dev;
 
 	rpm->kdev = kdev;
+#endif
 	rpm->available = HAS_RUNTIME_PM(i915);
 
 	init_intel_runtime_pm_wakeref(rpm);
-#endif
 }
