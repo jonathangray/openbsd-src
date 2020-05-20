a31c1cc38fb56ea4b36094a2bc3d8237af96eef7
Jonathan Gray <jsg@jsg.id.au>
2020-05-20 16:39:06 +1000

    add xa_mk_value() xa_is_value() xa_to_value()

--- sys/dev/pci/newdrm/include/linux/xarray.h
+++ sys/dev/pci/newdrm/include/linux/xarray.h
@@ -5,4 +5,25 @@
 
 #include <linux/gfp.h>
 
+static inline void *
+xa_mk_value(unsigned long v)
+{
+	unsigned long r = (v << 1) | 1;
+	return (void *)r;
+}
+
+static inline bool
+xa_is_value(const void *e)
+{
+	unsigned long v = (unsigned long)e;
+	return v & 1;
+}
+
+static inline unsigned long
+xa_to_value(const void *e)
+{
+	unsigned long v = (unsigned long)e;
+	return v >> 1;
+}
+
 #endif
