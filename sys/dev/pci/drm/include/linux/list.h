93e1eae1e53bbaaacefac61a7a12d6c4a61c366b
Jonathan Gray <jsg@jsg.id.au>
2022-01-14 12:51:45 +1100

    replace drm dir with newdrm

--- sys/dev/pci/drm/include/linux/list.h
+++ sys/dev/pci/drm/include/linux/list.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: list.h,v 1.4 2021/10/01 04:36:38 jsg Exp $	*/
+/*	$OpenBSD: list.h,v 1.3 2021/07/07 02:38:36 jsg Exp $	*/
 /* drm_linux_list.h -- linux list functions for the BSDs.
  * Created: Mon Apr 7 14:30:16 1999 by anholt@FreeBSD.org
  */
