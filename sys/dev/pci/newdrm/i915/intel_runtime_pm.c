e6aa1b8c703ed6d252d734ab87bd853bd3e7411f
Anshuman Gupta <anshuman.gupta@intel.com>
2022-10-27 14:52:42 +0530

    drm/i915/dgfx: Grab wakeref at i915_ttm_unmap_virtual
    [ Upstream commit 1cacd6894d5f4084f1581435e92d8a18d6721b25 ]
    
    We had already grabbed the rpm wakeref at obj destruction path,
    but it also required to grab the wakeref when object moves.
    When i915_gem_object_release_mmap_offset() gets called by
    i915_ttm_move_notify(), it will release the mmap offset without
    grabbing the wakeref. We want to avoid that therefore,
    grab the wakeref at i915_ttm_unmap_virtual() accordingly.
    
    While doing that also changed the lmem_userfault_lock from
    mutex to spinlock, as spinlock widely used for list.
    
    Also changed if (obj->userfault_count) to
    GEM_BUG_ON(!obj->userfault_count).
    
    v2:
    - Removed lmem_userfault_{list,lock} from intel_gt. [Matt Auld]
    
    Fixes: ad74457a6b5a ("drm/i915/dgfx: Release mmap on rpm suspend")
    Suggested-by: Matthew Auld <matthew.auld@intel.com>
    Signed-off-by: Anshuman Gupta <anshuman.gupta@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221027092242.1476080-3-anshuman.gupta@intel.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sys/dev/pci/newdrm/i915/intel_runtime_pm.c
+++ sys/dev/pci/newdrm/i915/intel_runtime_pm.c
@@ -657,6 +657,6 @@ void intel_runtime_pm_init_early(struct intel_runtime_pm *rpm)
 
 	init_intel_runtime_pm_wakeref(rpm);
 	INIT_LIST_HEAD(&rpm->lmem_userfault_list);
-	mutex_init(&rpm->lmem_userfault_lock);
+	mtx_init(&rpm->lmem_userfault_lock, IPL_NONE);
 	intel_wakeref_auto_init(&rpm->userfault_wakeref, rpm);
 }
