a051cde7d2d51d1b7e23032d6dfb8c43e1cd967b
Jonathan Gray <jsg@jsg.id.au>
2022-11-23 13:28:23 +1100

    make amdgpu_xgmi.c build

--- sys/dev/pci/newdrm/include/linux/sysfs.h
+++ sys/dev/pci/newdrm/include/linux/sysfs.h
@@ -19,6 +19,8 @@ struct attribute_group {
 	struct bin_attribute **bin_attrs;
 };
 
+#define ATTRIBUTE_GROUPS(x)
+
 #define sysfs_create_link(x, y, z)	0
 #define sysfs_remove_link(x, y)
 #define sysfs_create_group(x, y)	0
