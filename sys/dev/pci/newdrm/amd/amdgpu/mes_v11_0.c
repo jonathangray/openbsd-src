f558e0697036866050c37a0205313d4d10376845
Jonathan Gray <jsg@jsg.id.au>
2022-11-23 11:37:05 +1100

    make mes_v11_0.c build

--- sys/dev/pci/newdrm/amd/amdgpu/mes_v11_0.c
+++ sys/dev/pci/newdrm/amd/amdgpu/mes_v11_0.c
@@ -646,7 +646,7 @@ static void mes_v11_0_enable(struct amdgpu_device *adev, bool enable)
 		WREG32_SOC15(GC, 0, regCP_MES_CNTL, data);
 
 		if (amdgpu_emu_mode)
-			msleep(100);
+			drm_msleep(100);
 		else
 			udelay(50);
 	} else {
@@ -1014,7 +1014,7 @@ static int mes_v11_0_ring_init(struct amdgpu_device *adev)
 	ring->doorbell_index = adev->doorbell_index.mes_ring0 << 1;
 	ring->eop_gpu_addr = adev->mes.eop_gpu_addr[AMDGPU_MES_SCHED_PIPE];
 	ring->no_scheduler = true;
-	sprintf(ring->name, "mes_%d.%d.%d", ring->me, ring->pipe, ring->queue);
+	snprintf(ring->name, sizeof(ring->name), "mes_%d.%d.%d", ring->me, ring->pipe, ring->queue);
 
 	return amdgpu_ring_init(adev, ring, 1024, NULL, 0,
 				AMDGPU_RING_PRIO_DEFAULT, NULL);
@@ -1024,7 +1024,7 @@ static int mes_v11_0_kiq_ring_init(struct amdgpu_device *adev)
 {
 	struct amdgpu_ring *ring;
 
-	spin_lock_init(&adev->gfx.kiq.ring_lock);
+	mtx_init(&adev->gfx.kiq.ring_lock, IPL_TTY);
 
 	ring = &adev->gfx.kiq.ring;
 
@@ -1038,7 +1038,7 @@ static int mes_v11_0_kiq_ring_init(struct amdgpu_device *adev)
 	ring->doorbell_index = adev->doorbell_index.mes_ring1 << 1;
 	ring->eop_gpu_addr = adev->mes.eop_gpu_addr[AMDGPU_MES_KIQ_PIPE];
 	ring->no_scheduler = true;
-	sprintf(ring->name, "mes_kiq_%d.%d.%d",
+	snprintf(ring->name, sizeof(ring->name), "mes_kiq_%d.%d.%d",
 		ring->me, ring->pipe, ring->queue);
 
 	return amdgpu_ring_init(adev, ring, 1024, NULL, 0,
