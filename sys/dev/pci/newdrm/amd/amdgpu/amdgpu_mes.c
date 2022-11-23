7e6778f856919ed61c8d3b70c0d96e4ca4991fbf
Jonathan Gray <jsg@jsg.id.au>
2022-11-23 12:58:27 +1100

    make amdgpu_mes.c build

--- sys/dev/pci/newdrm/amd/amdgpu/amdgpu_mes.c
+++ sys/dev/pci/newdrm/amd/amdgpu/amdgpu_mes.c
@@ -160,9 +160,9 @@ int amdgpu_mes_init(struct amdgpu_device *adev)
 	idr_init(&adev->mes.gang_id_idr);
 	idr_init(&adev->mes.queue_id_idr);
 	ida_init(&adev->mes.doorbell_ida);
-	spin_lock_init(&adev->mes.queue_id_lock);
-	spin_lock_init(&adev->mes.ring_lock);
-	mutex_init(&adev->mes.mutex_hidden);
+	mtx_init(&adev->mes.queue_id_lock, IPL_TTY);
+	mtx_init(&adev->mes.ring_lock, IPL_TTY);
+	rw_init(&adev->mes.mutex_hidden, "agmes");
 
 	adev->mes.total_max_queue = AMDGPU_FENCE_MES_QUEUE_ID_MASK;
 	adev->mes.vmid_mask_mmhub = 0xffffff00;
