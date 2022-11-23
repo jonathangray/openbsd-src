eb38aaad8117c21ce5a2d96d852d878254233e8c
Jonathan Gray <jsg@jsg.id.au>
2022-11-23 13:00:47 +1100

    make amdgpu_ras.c build

--- sys/dev/pci/newdrm/include/linux/device.h
+++ sys/dev/pci/newdrm/include/linux/device.h
@@ -85,6 +85,9 @@ struct device_attribute {
 #define dev_dbg(dev, fmt, arg...)				\
 	printf("drm:pid%d:%s *DEBUG* " fmt, curproc->p_p->ps_pid,	\
 	    __func__ , ## arg)
+#define dev_dbg_once(dev, fmt, arg...)				\
+	printf("drm:pid%d:%s *DEBUG* " fmt, curproc->p_p->ps_pid,	\
+	    __func__ , ## arg)
 #define dev_dbg_ratelimited(dev, fmt, arg...)			\
 	printf("drm:pid%d:%s *DEBUG* " fmt, curproc->p_p->ps_pid,	\
 	    __func__ , ## arg)
@@ -94,6 +97,8 @@ struct device_attribute {
 #define dev_info_once(dev, fmt, arg...) 			\
 	    do { } while(0)
 #define dev_dbg(dev, fmt, arg...) 				\
+	    do { } while(0)
+#define dev_dbg_once(dev, fmt, arg...) 				\
 	    do { } while(0)
 #define dev_dbg_ratelimited(dev, fmt, arg...) 			\
 	    do { } while(0)
