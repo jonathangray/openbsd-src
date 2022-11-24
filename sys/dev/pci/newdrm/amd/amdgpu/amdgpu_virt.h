6b65e0a22bbbc0bcc1f44f3efc4c5e0e8c79ee9d
Jonathan Gray <jsg@jsg.id.au>
2022-11-24 19:48:37 +1100

    skip amdgpu is_kernel_in_hyp_mode() call

--- sys/dev/pci/newdrm/amd/amdgpu/amdgpu_virt.h
+++ sys/dev/pci/newdrm/amd/amdgpu/amdgpu_virt.h
@@ -305,7 +305,7 @@ static inline bool is_virtual_machine(void)
 {
 #if defined(CONFIG_X86)
 	return boot_cpu_has(X86_FEATURE_HYPERVISOR);
-#elif defined(CONFIG_ARM64)
+#elif defined(CONFIG_ARM64) && defined(notyet)
 	return !is_kernel_in_hyp_mode();
 #else
 	return false;
