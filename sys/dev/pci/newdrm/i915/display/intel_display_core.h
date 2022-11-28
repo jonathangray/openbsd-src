f3385282b232b2c5154bde3145c950520d8f16fb
Jonathan Gray <jsg@jsg.id.au>
2022-11-28 15:44:41 +1100

    make icl_dsi.c build

--- sys/dev/pci/newdrm/i915/display/intel_display_core.h
+++ sys/dev/pci/newdrm/i915/display/intel_display_core.h
@@ -90,7 +90,7 @@ struct intel_audio {
 	struct i915_audio_component *component;
 	bool component_registered;
 	/* mutex for audio/video sync */
-	struct mutex mutex;
+	struct rwlock mutex;
 	int power_refcount;
 	u32 freq_cntrl;
 
@@ -110,7 +110,7 @@ struct intel_audio {
  * dpll, because on some platforms plls share registers.
  */
 struct intel_dpll {
-	struct mutex lock;
+	struct rwlock lock;
 
 	int num_shared_dpll;
 	struct intel_shared_dpll shared_dplls[I915_NUM_PLLS];
@@ -238,7 +238,7 @@ struct intel_wm {
 	 * protects * intel_crtc->wm.active and
 	 * crtc_state->wm.need_postvbl_update.
 	 */
-	struct mutex wm_mutex;
+	struct rwlock wm_mutex;
 
 	bool ipc_enabled;
 };
@@ -279,7 +279,7 @@ struct intel_display {
 
 	struct {
 		/* backlight registers and fields in struct intel_panel */
-		struct mutex lock;
+		struct rwlock lock;
 	} backlight;
 
 	struct {
@@ -350,7 +350,7 @@ struct intel_display {
 		 * gmbus.mutex protects against concurrent usage of the single
 		 * hw gmbus controller on different i2c buses.
 		 */
-		struct mutex mutex;
+		struct rwlock mutex;
 
 		struct intel_gmbus *bus[GMBUS_NUM_PINS];
 
@@ -362,7 +362,7 @@ struct intel_display {
 		bool comp_added;
 
 		/* Mutex to protect the above hdcp component related values. */
-		struct mutex comp_mutex;
+		struct rwlock comp_mutex;
 	} hdcp;
 
 	struct {
@@ -379,7 +379,7 @@ struct intel_display {
 		u32 mmio_base;
 
 		/* protects panel power sequencer state */
-		struct mutex mutex;
+		struct rwlock mutex;
 	} pps;
 
 	struct {
