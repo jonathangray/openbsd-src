229ec869bba3b129a23ab2043b4010cd06abcfd0
Jonathan Gray <jsg@jsg.id.au>
2022-11-28 22:41:57 +1100

    make i915_utils.c build

--- sys/dev/pci/newdrm/i915/i915_utils.c
+++ sys/dev/pci/newdrm/i915/i915_utils.c
@@ -129,9 +129,12 @@ void set_timer_ms(struct timeout *t, unsigned long timeout)
 
 bool i915_vtd_active(struct drm_i915_private *i915)
 {
+	return false;
+#ifdef notyet
 	if (device_iommu_mapped(i915->drm.dev))
 		return true;
 
 	/* Running as a guest, we assume the host is enforcing VT'd */
 	return i915_run_as_guest();
+#endif
 }
