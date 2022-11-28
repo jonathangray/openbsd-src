bdc4a7a54a98022d4cc7c100e9f0636c651a52a8
Jonathan Gray <jsg@jsg.id.au>
2022-11-28 18:14:12 +1100

    add lockdep_assert()

--- sys/dev/pci/newdrm/include/linux/lockdep.h
+++ sys/dev/pci/newdrm/include/linux/lockdep.h
@@ -13,6 +13,7 @@ struct pin_cookie {
 
 #define might_lock(lock)
 #define might_lock_nested(lock, subc)
+#define lockdep_assert(c)		do {} while(0)
 #define lockdep_assert_held(lock)	do { (void)(lock); } while(0)
 #define lockdep_assert_held_once(lock)	do { (void)(lock); } while(0)
 #define lockdep_assert_once(lock)	do { (void)(lock); } while(0)
