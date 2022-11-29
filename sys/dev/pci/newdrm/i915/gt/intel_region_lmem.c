351de5dac503a954b8cc77b30c598ab2a7510804
Jonathan Gray <jsg@jsg.id.au>
2022-11-29 21:34:39 +1100

    make intel_region_lmem.c build

--- sys/dev/pci/newdrm/i915/gt/intel_region_lmem.c
+++ sys/dev/pci/newdrm/i915/gt/intel_region_lmem.c
@@ -20,17 +20,22 @@
 #ifdef CONFIG_64BIT
 static void _release_bars(struct pci_dev *pdev)
 {
+	STUB();
+#ifdef notyet
 	int resno;
 
 	for (resno = PCI_STD_RESOURCES; resno < PCI_STD_RESOURCE_END; resno++) {
 		if (pci_resource_len(pdev, resno))
 			pci_release_resource(pdev, resno);
 	}
+#endif
 }
 
 static void
 _resize_bar(struct drm_i915_private *i915, int resno, resource_size_t size)
 {
+	STUB();
+#ifdef notyet
 	struct pci_dev *pdev = i915->drm.pdev;
 	int bar_size = pci_rebar_bytes_to_size(size);
 	int ret;
@@ -45,10 +50,13 @@ _resize_bar(struct drm_i915_private *i915, int resno, resource_size_t size)
 	}
 
 	drm_info(&i915->drm, "BAR%d resized to %dM\n", resno, 1 << bar_size);
+#endif
 }
 
 static void i915_resize_lmem_bar(struct drm_i915_private *i915, resource_size_t lmem_size)
 {
+	STUB();
+#ifdef notyet
 	struct pci_dev *pdev = i915->drm.pdev;
 	struct pci_bus *root = pdev->bus;
 	struct resource *root_res;
@@ -111,6 +119,7 @@ static void i915_resize_lmem_bar(struct drm_i915_private *i915, resource_size_t 
 
 	pci_assign_unassigned_bus_resources(pdev->bus);
 	pci_write_config_dword(pdev, PCI_COMMAND, pci_cmd);
+#endif
 }
 #else
 static void i915_resize_lmem_bar(struct drm_i915_private *i915, resource_size_t lmem_size) {}
@@ -196,6 +205,9 @@ static int reserve_lowmem_region(struct intel_uncore *uncore,
 
 static struct intel_memory_region *setup_lmem(struct intel_gt *gt)
 {
+	STUB();
+	return ERR_PTR(-ENOSYS);
+#ifdef notyet
 	struct drm_i915_private *i915 = gt->i915;
 	struct intel_uncore *uncore = gt->uncore;
 	struct pci_dev *pdev = i915->drm.pdev;
@@ -286,6 +298,7 @@ static struct intel_memory_region *setup_lmem(struct intel_gt *gt)
 err_region_put:
 	intel_memory_region_destroy(mem);
 	return ERR_PTR(err);
+#endif
 }
 
 struct intel_memory_region *intel_gt_setup_lmem(struct intel_gt *gt)
