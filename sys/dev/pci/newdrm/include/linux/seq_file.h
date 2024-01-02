8edfd2bd44e288a0e8baf1cb3e90a42a0247b884
Jonathan Gray <jsg@jsg.id.au>
2024-01-02 18:48:26 +1100

    add seq_write()

--- sys/dev/pci/newdrm/include/linux/seq_file.h
+++ sys/dev/pci/newdrm/include/linux/seq_file.h
@@ -11,9 +11,18 @@ struct seq_file {
 };
 
 static inline void
-seq_printf(struct seq_file *m, const char *fmt, ...) {};
+seq_printf(struct seq_file *m, const char *fmt, ...)
+{
+}
 
 static inline void
-seq_puts(struct seq_file *m, const char *s) {};
+seq_puts(struct seq_file *m, const char *s)
+{
+}
+
+static inline void
+seq_write(struct seq_file *m, const void *p, size_t s)
+{
+}
 
 #endif
