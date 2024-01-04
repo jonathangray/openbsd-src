67829bdc00513c34ce295cadf9e9e02c355fa113
Jonathan Gray <jsg@jsg.id.au>
2024-01-04 17:22:01 +1100

    define CONFIG_DRM_AMD_DC_FP on x86

--- sys/dev/pci/newdrm/include/generated/autoconf.h
+++ sys/dev/pci/newdrm/include/generated/autoconf.h
@@ -10,6 +10,7 @@
 #define CONFIG_DRM_AMD_DC			1
 #if defined(__amd64__) || defined(__i386__)
 #define CONFIG_DRM_AMD_DC_DCN			1
+#define CONFIG_DRM_AMD_DC_FP			1
 #endif
 #if 0
 #define CONFIG_DRM_AMDGPU_SI			1
