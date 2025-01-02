2aa8343a382d3307f6a78fff841c0c0c0424109a
Jonathan Gray <jsg@jsg.id.au>
2025-01-02 20:51:49 +1100

    add DECLARE_RWSEM()

--- sys/dev/pci/newdrm/include/linux/rwsem.h
+++ sys/dev/pci/newdrm/include/linux/rwsem.h
@@ -12,4 +12,7 @@
 #define up_write(rwl)			rw_exit_write(rwl)
 #define downgrade_write(rwl)		rw_enter(rwl, RW_DOWNGRADE)
 
+#define DECLARE_RWSEM(rwl) \
+	struct rwlock rwl = RWLOCK_INITIALIZER(#rwl)
+
 #endif
