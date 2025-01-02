e6057da2af7b4a4066db9a8786b011c90a880d46
Jonathan Gray <jsg@jsg.id.au>
2025-01-02 15:06:33 +1100

    add __nonstring

--- sys/dev/pci/newdrm/include/linux/compiler.h
+++ sys/dev/pci/newdrm/include/linux/compiler.h
@@ -18,6 +18,7 @@
 #define __init
 #define __exit
 #define __deprecated
+#define __nonstring
 #define __always_unused	__attribute__((__unused__))
 #define __maybe_unused	__attribute__((__unused__))
 #define __always_inline	inline __attribute__((__always_inline__))
