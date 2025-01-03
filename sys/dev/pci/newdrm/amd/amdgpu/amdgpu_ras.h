a557a46afbf760dfd18a65ae40e357778049663c
Jonathan Gray <jsg@jsg.id.au>
2025-01-03 15:05:35 +1100

    mutex -> rwlock for amdgpu_ras

--- sys/dev/pci/newdrm/amd/amdgpu/amdgpu_ras.h
+++ sys/dev/pci/newdrm/amd/amdgpu/amdgpu_ras.h
@@ -485,7 +485,7 @@ struct ras_ecc_err {
 };
 
 struct ras_ecc_log_info {
-	struct mutex lock;
+	struct rwlock lock;
 	struct radix_tree_root de_page_tree;
 	uint64_t	de_queried_count;
 	uint64_t	prev_de_queried_count;
@@ -549,7 +549,7 @@ struct amdgpu_ras {
 
 	struct task_struct *page_retirement_thread;
 	wait_queue_head_t page_retirement_wq;
-	struct mutex page_retirement_lock;
+	struct rwlock page_retirement_lock;
 	atomic_t page_retirement_req_cnt;
 	atomic_t poison_creation_count;
 	struct mutex page_rsv_lock;
