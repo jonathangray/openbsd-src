11a9e1fbf79975f4c1511517ff06dd48117c4a95
Jonathan Gray <jsg@jsg.id.au>
2025-01-03 14:10:29 +1100

    make amdgpu_gmc.c build

--- sys/dev/pci/newdrm/amd/amdgpu/amdgpu_gmc.c
+++ sys/dev/pci/newdrm/amd/amdgpu/amdgpu_gmc.c
@@ -789,7 +789,7 @@ void amdgpu_gmc_fw_reg_write_reg_wait(struct amdgpu_device *adev,
 	while (r < 1 && cnt++ < MAX_KIQ_REG_TRY &&
 	       !amdgpu_reset_pending(adev->reset_domain)) {
 
-		msleep(MAX_KIQ_REG_BAILOUT_INTERVAL);
+		drm_msleep(MAX_KIQ_REG_BAILOUT_INTERVAL);
 		r = amdgpu_fence_wait_polling(ring, seq, MAX_KIQ_REG_WAIT);
 	}
 
