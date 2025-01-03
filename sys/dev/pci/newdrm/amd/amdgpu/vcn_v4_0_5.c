8598d9a7319c257fb8f324e558a741c9dd8e0c49
Jonathan Gray <jsg@jsg.id.au>
2025-01-03 13:52:30 +1100

    make vcn_v4_0_5.c build

--- sys/dev/pci/newdrm/amd/amdgpu/vcn_v4_0_5.c
+++ sys/dev/pci/newdrm/amd/amdgpu/vcn_v4_0_5.c
@@ -175,7 +175,7 @@ static int vcn_v4_0_5_sw_init(void *handle)
 			ring->doorbell_index = (adev->doorbell_index.vcn.vcn_ring0_1 << 1) +
 						2 + 8 * i;
 		ring->vm_hub = AMDGPU_MMHUB0(0);
-		sprintf(ring->name, "vcn_unified_%d", i);
+		snprintf(ring->name, sizeof(ring->name), "vcn_unified_%d", i);
 
 		r = amdgpu_ring_init(adev, ring, 512, &adev->vcn.inst[i].irq, 0,
 				AMDGPU_RING_PRIO_0, &adev->vcn.inst[i].sched_score);
@@ -1100,7 +1100,7 @@ static int vcn_v4_0_5_start(struct amdgpu_device *adev)
 					break;
 				mdelay(10);
 				if (amdgpu_emu_mode == 1)
-					msleep(1);
+					drm_msleep(1);
 			}
 
 			if (amdgpu_emu_mode == 1) {
