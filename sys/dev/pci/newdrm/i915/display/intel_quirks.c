3509316c7889fa07e38611a3db66071f02718037
Jonathan Gray <jsg@jsg.id.au>
2025-01-06 15:08:05 +1100

    make intel_quirks.c build

--- sys/dev/pci/newdrm/i915/display/intel_quirks.c
+++ sys/dev/pci/newdrm/i915/display/intel_quirks.c
@@ -245,7 +245,7 @@ static struct intel_dpcd_quirk intel_dpcd_quirks[] = {
 
 void intel_init_quirks(struct intel_display *display)
 {
-	struct pci_dev *d = i915->drm.pdev;
+	struct pci_dev *d = display->drm->pdev;
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(intel_quirks); i++) {
@@ -268,7 +268,7 @@ void intel_init_dpcd_quirks(struct intel_dp *intel_dp,
 			    const struct drm_dp_dpcd_ident *ident)
 {
 	struct intel_display *display = to_intel_display(intel_dp);
-	struct pci_dev *d = to_pci_dev(display->drm->dev);
+	struct pci_dev *d = display->drm->pdev;
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(intel_dpcd_quirks); i++) {
