dea4b799c5eded5c23010ad67bad21e94ccd2323
Jonathan Gray <jsg@jsg.id.au>
2025-01-06 16:34:09 +1100

    make intel_dmc_wl.c build

--- sys/dev/pci/newdrm/i915/display/intel_dmc_wl.c
+++ sys/dev/pci/newdrm/i915/display/intel_dmc_wl.c
@@ -128,7 +128,7 @@ void intel_dmc_wl_init(struct intel_display *display)
 		return;
 
 	INIT_DELAYED_WORK(&wl->work, intel_dmc_wl_work);
-	spin_lock_init(&wl->lock);
+	mtx_init(&wl->lock, IPL_TTY);
 	refcount_set(&wl->refcount, 0);
 }
 
