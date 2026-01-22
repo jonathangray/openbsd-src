8821587079a638466176a87385967e45f0e7ceba
Jonathan Gray <jsg@jsg.id.au>
2026-01-23 10:41:48 +1100

    make ttm_resource.c build

--- sys/dev/pci/newdrm/include/linux/cgroup_dmem.h
+++ sys/dev/pci/newdrm/include/linux/cgroup_dmem.h
@@ -3,6 +3,8 @@
 #ifndef _LINUX_CGROUP_DMEM_H
 #define _LINUX_CGROUP_DMEM_H
 
+struct dmem_cgroup_pool_state;
+
 static inline void *
 dmem_cgroup_register_region(uint64_t size, const char *fmt, ...)
 {
@@ -15,13 +17,30 @@ dmem_cgroup_unregister_region(void *r)
 }
 
 static inline bool
-dmem_cgroup_state_evict_valuable(void *a, void *b, bool c, bool *d)
+dmem_cgroup_state_evict_valuable(struct dmem_cgroup_pool_state *l,
+    struct dmem_cgroup_pool_state *t, bool a, bool *b)
 {
 	return true;
 }
 
 static inline void
-dmem_cgroup_pool_state_put(void *a)
+dmem_cgroup_pool_state_put(struct dmem_cgroup_pool_state *p)
+{
+}
+
+static inline int
+dmem_cgroup_try_charge(void *a, uint64_t size,
+    struct dmem_cgroup_pool_state **r, struct dmem_cgroup_pool_state **p)
+{
+	if (r)
+		*r = NULL;
+	if (p)
+		*p = NULL;
+	return 0;
+}
+
+static inline void
+dmem_cgroup_uncharge(struct dmem_cgroup_pool_state *p, uint64_t size)
 {
 }
 
