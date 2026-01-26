865a4c53a8c86e6ecae5f726570b0e67076b0379
Jonathan Gray <jsg@jsg.id.au>
2026-01-26 17:47:56 +1100

    make amdgpu_sdma.c build

--- sys/dev/pci/newdrm/amd/amdgpu/amdgpu_sdma.c
+++ sys/dev/pci/newdrm/amd/amdgpu/amdgpu_sdma.c
@@ -493,10 +493,12 @@ void amdgpu_sdma_sysfs_reset_mask_fini(struct amdgpu_device *adev)
 	if (!amdgpu_gpu_recovery)
 		return;
 
+#ifdef notyet
 	if (adev->dev->kobj.sd) {
 		if (adev->sdma.num_instances)
 			device_remove_file(adev->dev, &dev_attr_sdma_reset_mask);
 	}
+#endif
 }
 
 struct amdgpu_ring *amdgpu_sdma_get_shared_ring(struct amdgpu_device *adev, struct amdgpu_ring *ring)
