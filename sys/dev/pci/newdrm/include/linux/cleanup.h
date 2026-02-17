b393be224ce44f1d4bc9f895a3b874dfb2ffbc09
Jonathan Gray <jsg@jsg.id.au>
2026-02-17 17:47:11 +1100

    add DEFINE_CLASS()

--- sys/dev/pci/newdrm/include/linux/cleanup.h
+++ sys/dev/pci/newdrm/include/linux/cleanup.h
@@ -17,4 +17,19 @@ __free_##fn(void *p)			\
 #define no_free_ptr(p) \
 	({void *_p = (p); (p) = NULL; _p; })
 
+#define DEFINE_CLASS(_name, _type, _exit, _enter, _args...)	\
+typedef _type class_##_name##_t;				\
+static inline _type						\
+class_##_name##_constructor(_args)				\
+{								\
+	_type t = _enter;					\
+	return t;						\
+}								\
+static inline void						\
+class_##_name##_destructor(_type *p)				\
+{								\
+	_type _T = *p;						\
+	_exit;							\
+}
+
 #endif
